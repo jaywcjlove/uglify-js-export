1e70603bbf74ca16c2cd05bd3dcc2a7f8ad190a6
jaywcjlove <398188662@qq.com>
2022-08-11 14:50:00 +0800

    test: add test case.

--- test/uglifyjs.test.js
+++ test/uglifyjs.test.js
@@ -8,3 +8,21 @@ test('minify', () => {
   expect(typeof minify).toBe('function');
   expect(minify(code).code).toBe("function Good(n){return\"Hi!, \"+n}");
 });
+
+const code2 = `function Good(name) {
+  var ab = 'good.'
+  return 'Hi!, ' + name + ab
+}`;
+
+test('minify 2', () => {
+  expect(minify(code2).code).toBe("function Good(o){return\"Hi!, \"+o+\"good.\"}");
+});
+
+const code3 = `function Good(name) {
+  var ab = 'good.'
+  return 'Hi!, ' + name + ab
+}`;
+
+test('minify 3', () => {
+  expect(minify(code3, { compress: { unused: false }}).code).toBe("function Good(o){var n;return\"Hi!, \"+o+\"good.\"}");
+});
